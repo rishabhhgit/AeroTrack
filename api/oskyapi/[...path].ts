cd12e30350947114c60a0d99e8dff9bfe81b32d4
rishabh <rishabhh.steam@gmail.com>
2026-07-14 16:40:43 +0530

    fix CORS Authorization header in oskyapi proxy

--- api/oskyapi/[...path].ts
+++ api/oskyapi/[...path].ts
@@ -5,7 +5,7 @@ const OPENSKY_BASE = 'https://opensky-network.org/api';
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') {
     res.status(200).end();
